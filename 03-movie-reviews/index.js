600006cfc4731ff5a7771422093f422859c3136d
Atomicnaut <cgseven@me.com>
2025-02-06 18:59:19 -0600

    Clean up tree

--- 03-movie-reviews/index.js
+++
@@ -1,6 +0,0 @@
-import app from "./server.js";
-import mongodb from "mongodb";
-import ReviewsDAO from "./dao/reviewsDAO.js";
-
-const MongoClient = mongodb.MongoClient;
-const uri = "mongodb+srv://atomicnaut:7321@cluster0.6zu8a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
